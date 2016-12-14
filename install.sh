eb3f462658c11c5a1267826f6a654d421496f9f5
Bruno de Lima e Silva <dlimaun@gmail.com>
2016-12-14 17:29:44 -0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -40,4 +40,16 @@ sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/mas
 
 # Install My custom zsh-theme
 mkdir ~/.oh-my-zsh/custom/themes
-curl -o ~/.oh-my-zsh/custom/themes/lima-agnoster.zsh-theme https://gist.githubusercontent.com/brunodles/9bb7c3ff4e2a7b9e9bddece9082c35ca/raw/lima-agnoster.zsh-theme
+curl -o ~/.oh-my-zsh/custom/themes/agnoster-lima.zsh-theme https://gist.githubusercontent.com/brunodles/9bb7c3ff4e2a7b9e9bddece9082c35ca/raw/lima-agnoster.zsh-theme
+
+# Install My .zshrc file
+curl -o ~/.zshrc https://gist.githubusercontent.com/brunodles/c0d5fa8dfddeb6af24dde4f1f00854a3/raw
+
+# Instal custom color theme
+wget http://download.opensuse.org/repositories/home:Horst3180/xUbuntu_15.10/Release.key
+sudo apt-key add - < Release.key
+sudo sh -c "echo 'deb http://download.opensuse.org/repositories/home:/Horst3180/xUbuntu_15.10/ /' >>  /etc/apt/sources.list.d/arc-theme.list"
+sudo add-apt-repository -y ppa:moka/stable
+sudo apt-get update
+sudo apt-get install -y arc-theme
+sudo apt-get install -y moka-icon-theme
